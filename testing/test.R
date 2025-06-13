9935b25406a4ec4aa43dab6b7d9c52ba319427bf
alexandre-combeau <alexandre.combeau.77@gmail.com>
2025-06-13 17:09:56 +0200

    Add files via upload

--- testing/test.R
+++ testing/test.R
@@ -1,24 +1,54 @@
-library(dust)
+#install.packages("devtools")
+#devtools::install_github("alexandre-combeau/changepoints")
+#devtools::install_github("vrunge/dust")
 library(changepoints)
+library(dust)
+gap <- 10
+#gap <- 1
+data <- dataGenerator_1D(chpts = c(50,100,200,300),
+                         parameters = c(0,gap,0,gap),
+                         sdNoise = 0)
+plot(data, type = 'b')
+n <- 300
+resOP <- optimal_partitioning(data, beta = 2*log(n))
+resDUST <- dust.1D(data, penalty = log(n))
 
-n <- 100
-x <- dataGenerator_1D(chpts = c(2, 4, 6) * n,
-                      parameters = c(5,10, 2),
-                      sdNoise = 1,
-                      type = "gauss")
-best_beta <- 2 * sdDiff(x, "HALL")^2 * log(length(x))
-OP_CPP <- rcpp_optimal_partitioning(x, best_beta)
+resOP$changepoints ### OK
+resDUST$changepoints ### OK
+(resOP$costQ - 2*(resDUST$costQ + cumsum(data^2)/2)) ### OK
 
-# Visualization
-cpts <- OP_CPP$changepoints
-plot(x, type = "o", main = "Optimal Partitioning avec R",
-     ylab = "Values", xlab = "Time", col = "blue")
-abline(v = cpts, col = "red", lwd = 2, lty = 2)
-cpts2 <- c(0,cpts)
-for (i in 1:(length(cpts2) - 1))
-{
-  segment_mean <- mean(x[(cpts2[i] + 1):cpts2[i + 1]])
-  segments(x0 = cpts2[i] + 1, y0 = segment_mean,
-           x1 = cpts2[i+1], y1 = segment_mean,
-           col = "green", lwd = 2)
-}
+
+# Example of SVP in OP mode
+resOP <- optimal_partitioning(data, beta = 2*log(n))
+resSVP_OP <- smallest_valid_partitioning_op_mode(data, gamma = 2*log(n))
+resOP$changepoints # OK
+resSVP_OP$changepoints # OK
+resOP$costQ # OK
+resSVP_OP$costQ # OK
+
+##########################################################################
+
+resSVP <- smallest_valid_partitioning(data, gamma = 2*log(n), valid_test = validity_function1)
+resSVP_OP$costQ
+resOP$costQ
+resSVP$costQ #### WHAT ??? WITH gap <- 10
+
+resOP$costQ - resSVP_OP$costQ
+resOP$costQ[n]
+resSVP$costQ[n]
+
+#####################
+
+# FAIS UN PETIT EXEMPLE AVEC DES PRINTS !!!
+
+data <- c(rep(5,5), rep(15,5))
+plot(data, type = 'b')
+n <- length(data)
+
+resSVP <- smallest_valid_partitioning(data, gamma = 2*log(n), valid_test = validity_function_range) # does not work
+resSVP <- smallest_valid_partitioning(data, gamma = 2*log(n), valid_test = validity_function2) # cost problem
+resSVP$changepoints
+resSVP$costQ
+
+
+#####################
